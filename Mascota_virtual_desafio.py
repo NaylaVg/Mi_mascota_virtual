0ec17b6625731891272953db284dd2e2b99a67d7
unknown <naylamaitevega@gmail.com>
2025-10-25 22:38:38 -0300

    Cree método constructor de nuestra MascotaVirtual método alimentar

--- Mascota_virtual_desafio.py
+++ Mascota_virtual_desafio.py
@@ -1,13 +1,34 @@
-# importar el modulo random
-# import imagen desde el archivo mascota.py
+import random
+
+from mascota import imagen
 
 
 class MascotaVirtual:
     def __init__(self, nombre):
-        pass
+        self.nombre = nombre
+        self.felicidad = 0
+        self.hambre = 0
+        self.imagen = imagen.inicio
+        self.imagen_triste = imagen.triste
+        self.imagen_feliz = imagen.feliz
+        self.imagen_muerto = imagen.muerto
+        self.imagen_disgustado = imagen.disgustado
 
     def alimentar(self):
-        pass
+        self.felicidad -= random.randint(5, 10)
+        if self.felicidad < 0:
+            self.felicidad = 0
+        if self.hambre == 0: 
+            print(self.imagen_disgustado)
+            print(self.nombre, "está lleno, no puede comer más!")
+        else:
+            self.hambre -= random.randint(5, 10)
+            if self.hambre < 0:
+                self.hambre = 0
+            print(self.imagen_feliz)
+            print(self.nombre, "ha sido alimentado!")
+
+  
 
     def jugar(self):
         pass
@@ -15,17 +36,17 @@ class MascotaVirtual:
     def estado_animo(self):
         pass
 
-    def presentacion(self):  # opcional
+    def banarse(self):
         pass
 
-    def despedida(self):  # opcional
+    def dormir(self):
         pass
 
+    def pasear(self):
+        pass
 
-# Crear una instancia de MascotaVirtual
-
-# Interactuar con la mascota virtual
-# alimenta, juega y muestra su estado de animo
-# repite esta accion al menos 8 veces
-
-# Si te animas crea una interfaz para poder interactuar con tu mascota
+    def presentacion(self):
+        pass
+ 
+    def despedida(self):
+        pass
